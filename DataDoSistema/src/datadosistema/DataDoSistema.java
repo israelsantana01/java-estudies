0a2e8c3e64a36c6c53ccfbd905079ca3827421b9
Shocker <israelofi2@gmail.com>
2019-10-22 17:03:56 -0300

    Create a repository that store java-programs

---
+++ DataDoSistema/src/datadosistema/DataDoSistema.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author shocker
+ */
+public class DataDoSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é: ");
+        System.out.println(relogio.toString());
+    }
+    
+}
---
+++ TestePrograma/src/testeprograma/TestePrograma.java
@@ -0,0 +1,11 @@
+
+package testeprograma;
+
+
+public class TestePrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+    
+}
