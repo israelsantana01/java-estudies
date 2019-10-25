09ce81e3692a116b6255ae1b573ea4f1d4ccec86
israelsantana01 <israelofi2@gmail.com>
2019-10-24 21:31:24 -0300

    Organize my repository.

---
+++ IdiomaDoSistema/src/idiomadosistema/IdiomaDoSistema.java
@@ -0,0 +1,15 @@
+
+package idiomadosistema;
+
+import java.util.Locale;
+
+
+
+public class IdiomaDoSistema {
+
+    public static void main(String[] args) {
+        Locale idioma = Locale.getDefault();
+        System.out.println(idioma.getDisplayLanguage());
+    }
+    
+}
---
+++ OperadoresAritmeticos/src/operadores/Operadores.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadores;
+
+/**
+ *
+ * @author shocker
+ */
+public class Operadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int x = 4;
+        *x += 2;
+        *System.out.println(x);
+        */
+        
+        /*float v = (float) 8.3;
+        *int ar = (int) Math.round(v);
+        *Sceilystem.out.println(ar);
+        */
+        
+        double ale = Math.random();
+        int n = (int) (15 + ale * (50-15));
+        System.out.println(n);
+}
+
+    
+}
