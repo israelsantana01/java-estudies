3a989b5ab2102ac1e0aea50477a7ff68ddf808b5
israelsantana01 <israelofi2@gmail.com>
2019-10-26 14:10:59 -0300

    Add more one java program

---
+++ RepetitionForG/src/repetitionforg/RepetitionForG.java
@@ -0,0 +1,15 @@
+
+package repetitionforg;
+
+
+public class RepetitionForG {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc = 0; cc<=100; cc += 10){
+            System.out.println(cc);
+        }
+    }
+    
+}
