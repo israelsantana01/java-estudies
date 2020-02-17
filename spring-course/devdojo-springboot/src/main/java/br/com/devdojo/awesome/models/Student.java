d8a8d146192ad3e7af28fd5af22b0cf5eb80c96f
israelsantana01 <israel.silva.santana09@gmail.com>
2020-02-17 16:10:46 -0300

    Add the folder with name Spring Course.

---
+++ spring-course/devdojo-springboot/src/main/java/br/com/devdojo/awesome/models/Student.java
@@ -0,0 +1,18 @@
+package br.com.devdojo.awesome.models;
+
+public class Student {
+	
+	private String name;
+	
+	public Student(String name) {
+		this.name = name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
